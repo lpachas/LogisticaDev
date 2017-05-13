bfdeb6342432fd7db90c94163597abb9d1e42742
Leiner Pachas <lpachas.73@gmail.com>
2017-05-13 18:57:25 -0500

    Mod del Form Venta/Create, añadiendo Tipo de Venta, y Forma de Pago

--- resources/views/ventas/venta/create.blade.php
+++ resources/views/ventas/venta/create.blade.php
@@ -10,60 +10,110 @@
                     </div>
                     <div class="x_content">
                         <form>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Cliente:</label>
-                                    <select id="id_cliente" class="form-control selectpicker" data-live-search="true">
-                                        <option value="">--Seleccione un cliente--</option>
-                                        @foreach($clientes as $cliente)
-                                            <option value="{{$cliente->ID_Cliente}}">{{$cliente->Nombre}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-2" style="padding-top: 1.7em;">
-                                <div class="form-group">
-                                    <button id="btn_cliente" class="btn btn-primary"><i class="fa fa-plus"></i> Nuevo Cliente</button>
-                                </div>
-                            </div>
-                            <div class="col-md-5">
-                                <div class="form-group">
-                                    <label>Fecha:</label>
-                                    <div class="input-group date" id="fecha">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-calendar"></i>
-                                        </div>
-                                        <input type="text" class="form-control pull-right" id="fecha_venta" name="fecha_venta">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Tipo de Documento:</label>
-                                    <select id="id_documento" name="id_documento" class="form-control selectpicker" data-live-search="true">
-                                        <option value="">--Seleccione un documento--</option>
-                                        @foreach($documentos as $doc)
-                                        <option value="{{$doc->ID_Tipo_Documento}}_{{$doc->Serie}}_{{$doc->Numero}}">{{$doc->Descripcion_Doc}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Serie:</label>
-                                    <input type="text" id="serie" class="form-control" placeholder="Serie de Documento">
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label>Número:</label>
-                                    <input type="text" id="numero" class="form-control" placeholder="Número de Documento">
-                                </div>
-                            </div>
-                        </div>
+                         <div class="row">
+                             <div class="col-md-6">
+                                 <div class="panel panel-primary">
+                                     <div class="panel-body">
+                                         <div class="row">
+                                             <div class="col-md-8">
+                                                 <div class="form-group">
+                                                     <label>Cliente:</label>
+                                                     <select id="id_cliente" class="form-control selectpicker" data-live-search="true">
+                                                         <option value="">--Seleccione un cliente--</option>
+                                                         @foreach($clientes as $cliente)
+                                                             <option value="{{$cliente->ID_Cliente}}">{{$cliente->Nombre}}</option>
+                                                         @endforeach
+                                                     </select>
+                                                 </div>
+                                             </div>
+                                             <div class="col-md-4" style="padding-top: 1.7em;">
+                                                 <div class="form-group">
+                                                     <button id="btn_cliente" class="btn btn-primary"><i class="fa fa-plus"></i> Nuevo Cliente</button>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <div class="col-md-6">
+                                 <div class="panel panel-primary">
+                                     <div class="panel-body">
+                                         <div class="row">
+                                             <div class="col-md-6">
+                                                 <div class="form-group">
+                                                     <label>Tipo de Venta:</label>
+                                                     <select id="id_tipo_venta" class="form-control selectpicker" data-live-search="true">
+                                                         <option value="">--Seleccione Tipo Venta--</option>
+                                                         <option value="1">Contado</option>
+                                                         <option value="2">Crédito</option>
+                                                     </select>
+                                                 </div>
+                                             </div>
+                                             <div class="col-md-6">
+                                                 <div class="form-group">
+                                                     <label>Fecha:</label>
+                                                     <div class="input-group date" id="fecha">
+                                                         <div class="input-group-addon">
+                                                             <i class="fa fa-calendar"></i>
+                                                         </div>
+                                                         <input type="text" class="form-control pull-right" id="fecha_venta" name="fecha_venta">
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="row">
+                             <div class="col-md-4">
+                               <div class="panel panel-primary">
+                                   <div class="panel-body">
+                                       <div class="col-md-12">
+                                           <div class="form-group">
+                                               <label>Forma de Pago:</label>
+                                               <select id="id_forma_pago" name="id_forma_pago" class="form-control selectpicker" data-live-search="true">
+                                                   <option value="">--Seleccione una forma--</option>
+                                                   <option value="">Forma 1</option>
+                                                   <option value="">Forma 2</option>
+                                               </select>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                             </div>
+                             <div class="col-md-8">
+                                 <div class="panel panel-primary">
+                                     <div class="panel-body">
+                                         <div class="row">
+                                             <div class="col-md-4">
+                                                 <div class="form-group">
+                                                     <label>Tipo de Documento:</label>
+                                                     <select id="id_documento" name="id_documento" class="form-control selectpicker" data-live-search="true">
+                                                         <option value="">--Seleccione un documento--</option>
+                                                         @foreach($documentos as $doc)
+                                                             <option value="{{$doc->ID_Tipo_Documento}}_{{$doc->Serie}}_{{$doc->Numero}}">{{$doc->Descripcion_Doc}}</option>
+                                                         @endforeach
+                                                     </select>
+                                                 </div>
+                                             </div>
+                                             <div class="col-md-4">
+                                                 <div class="form-group">
+                                                     <label>Serie:</label>
+                                                     <input type="text" id="serie" class="form-control" placeholder="Serie de Documento">
+                                                 </div>
+                                             </div>
+                                             <div class="col-md-4">
+                                                 <div class="form-group">
+                                                     <label>Número:</label>
+                                                     <input type="text" id="numero" class="form-control" placeholder="Número de Documento">
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
                         <div class="panel panel-primary">
                             <div class="panel-body">
                                 <div class="row">
